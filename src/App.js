0c7c1d085162dca04ad5dfc06b09f20caba1b5e0
prajwal joshi <prazwal@outlook.com>
2020-01-18 19:02:19 +1100

    app bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App =() => (
+    <div className="App">
+      <header className="App-header">
+        <p> hello world</p>
+      </header>
+    </div>
+  );
+
+
